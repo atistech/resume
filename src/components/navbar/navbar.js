ed4f197b34e8b35de94f6dbb035d1acda5073049
Atom <blue.atakan.14@gmail.com>
2020-04-01 23:06:47 +0300

    last changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -12,13 +12,13 @@ class Navbar extends Component {
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="matakansenturk.com">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
+        <a className="nav-link" href="matakansenturk.com">Features</a>
       </li>
       <li className="nav-item">
-        <a clclassNameass="nav-link" href="#">Pricing</a>
+        <a clclassNameass="nav-link" href="matakansenturk.com">Pricing</a>
       </li>
     </ul>
   </div>
