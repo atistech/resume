73311514b3d79519ad96a394a58c738c3b3b9880
Atom <blue.atakan.14@gmail.com>
2020-04-01 23:04:46 +0300

    last changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,21 +4,21 @@ class Navbar extends Component {
     state = {  }
     render() { 
         return ( 
-            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">matakansenturk.com</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand" href="#">matakansenturk.com</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Features</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+      <li className="nav-item">
+        <a clclassNameass="nav-link" href="#">Pricing</a>
       </li>
     </ul>
   </div>
